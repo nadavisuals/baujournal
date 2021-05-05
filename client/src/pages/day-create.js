4fc1faf8165398fbd9b726aafbd57206c3923c42
nadavisuals <44897257+nadavisuals@users.noreply.github.com>
2021-05-06 01:42:28 +0200

    constants, day-create page

---
+++ client/src/pages/day-create.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import NavBar from "../components/navbar.component";
+
+export default class DayCreate extends Component {
+  render() {
+    return (
+        
+      <div>
+          <NavBar />
+        <p>You are on the Day Create component!</p>
+      </div>
+    )
+  }
+}
