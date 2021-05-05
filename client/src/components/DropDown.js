44870ed60a2fe0476d4aa25c097a969b25465db2
nadavisuals <44897257+nadavisuals@users.noreply.github.com>
2021-05-06 00:22:04 +0200

    new pages added

---
+++ client/src/components/DropDown.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class DropDown extends Component {
+  render() {
+    return (
+        
+      <div>
+        <p><a href="/day-project-details">Projekt wählen.. V</a> </p>
+      </div>
+    )
+  }
+}
