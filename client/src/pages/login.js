aa886cdb7cb9097dda80b53401d9135431617ddb
nadavisuals <44897257+nadavisuals@users.noreply.github.com>
2021-05-19 22:44:55 +0200

    ui login form added

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,14 +1,51 @@
 import React from "react";
-import NavBar from "../components/NavBar";
+import { Form, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
   
   const Login = () => {
   
     return (
       <div>
-          <NavBar />
-        <p>You are on the Login component!</p>
+      <div className="conatiner">
+        <Form
+          style={{
+            maxWidth: "400px",
+            marginTop: "120px",
+            backgroundColor: " #737373",
+            padding: "50px",
+          }}
+          className="text-center container"
+        >
+          <Form.Group>
+            <h1>PITBAUjOURNAL</h1>
+    
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>User</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Email"
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter Password"
+            />
+          </Form.Group>
+          <Link
+              to={
+                "/day-choose-project"
+              }
+            >
+              <Button variant="dark">Login</Button>
+            </Link>
+        </Form>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Login;
