04da389baf056d5520c4be287677bbfa9d876c4b
nadavisuals <44897257+nadavisuals@users.noreply.github.com>
2021-05-21 21:09:58 +0200

    logout added

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,58 +1,61 @@
-import React from "react";
-import {  Navbar, Nav } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import React, { useContext } from 'react';
+import { Navbar, Nav } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+import userContext from "../context/userContext"
 import "../css/style.css";
 import "./navbar.css";
 
 const NavBar = () => {
-    return (
 
-        <Navbar
-        collapseOnSelect
-        expand="lg"
-        bg="secondary"
-        variant="dark"
-        className="top-nav"
-        >
-        <div className="container">
-          <Navbar.Brand>
-            <Link to="/day-choose-project">PITBAUjOURNAL</Link>
-          </Navbar.Brand>
-  
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto"></Nav>
-            <Nav>
-              <Nav.Link>
-                <Link to="/day-choose-project">Baujournal</Link>
-              </Nav.Link>
-  
-              <Nav.Link className="active" eventKey={2}>
-                <Link to="/projects">Projekte</Link>
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </div>
-      </Navbar>
+  const { userData, setUserData } = useContext(userContext);
 
-        );
-    };
-    
-    export default NavBar;
+  const history = useHistory();
+
+  const logout = () => {
+    setUserData({
+      token: undefined,
+      user: undefined,
+    });
+    localStorage.setItem("auth-token", "");
+    history.push("/");
+  };
 
 
+  return (
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="secondary"
+      variant="dark"
+      className="top-nav"
+    >
+      <div className="container">
+        <Navbar.Brand>
+          <Link to="/day-choose-project">PITBAUjOURNAL</Link>
+        </Navbar.Brand>
 
-    // const NavBar = () => {
-    //     return (
-    
-    //             <Navbar bg="white" variant="light">
-    //                 <Navbar.Brand href="/">PITBAUjOURNAL</Navbar.Brand>
-    //                 <Nav className="ml-auto">
-    //                     <Nav.Link href="/">Baujournal</Nav.Link>
-    //                     <Nav.Link href="/projects">Projekte</Nav.Link>
-    //                 </Nav>
-    //             </Navbar>
-    
-    //         );
-    //     };
-        
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto"></Nav>
+          <Nav>
+            <Nav.Link>
+              <Link to="/day-choose-project">Baujournal</Link>
+            </Nav.Link>
+
+            <Nav.Link className="active" eventKey={2}>
+              <Link to="/projects">Projekte</Link>
+            </Nav.Link>
+
+            <Nav.Link eventKey={2} onClick={() => logout()}>
+              <a>Logout</a>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </div>
+    </Navbar>
+
+  );
+};
+
+export default NavBar;
