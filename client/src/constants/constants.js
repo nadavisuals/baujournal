d78bf1c362abef64f8bd7f1fb776557f22805602
nadavisuals <44897257+nadavisuals@users.noreply.github.com>
2021-05-21 22:15:10 +0200

    netlify test

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,5 +1,6 @@
 const constants = {
-    backend_url: "http://localhost:5001",
-  };
-  
-  export default constants;
+  backend_url: "http://baujournal.herokuapp.com/",
+  backend_urlLocal: "http://localhost:5001",
+};
+
+export default constants;
